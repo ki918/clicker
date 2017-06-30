f7d53987063a0c5fa11c1f957892e3d5eb57c425
LHS <hslee@digicaps.com>
2017-06-30 17:28:06 +0900

    LHS
    UI 기본 구성 변경

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,13 +3,55 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
+	public static UIManager uiManager;
+
 	public Text goldDisplayer;
 	public Text goldPerDisplayer;
 	public Text goldPerSecDisplayer;
+
+	private ArrayList panelList;
+
+	public static UIManager getInstance() {
+		if (uiManager == null) {
+			uiManager = FindObjectOfType<UIManager> ();
+
+			if (uiManager == null) {
+				GameObject container = new GameObject ("UIManager");
+
+				uiManager = container.AddComponent<UIManager> ();
+			}
+		}
+
+		return uiManager;
+	}
+
+	void Start() {
+		panelList = new ArrayList ();
+		panelList.Add (GameObject.FindWithTag ("UpgradePanel"));
+		panelList.Add (GameObject.FindWithTag ("ItemPanel"));
+
+		for (int i = 1; i < panelList.Count; i++) {
+			GameObject obj = (GameObject)panelList [i];
+
+			obj.gameObject.SetActive (false);
+		}
+	}
 
 	void Update() {
 		goldDisplayer.text = "GOLD: " + DataController.getInstance ().getGold ();
 		goldPerDisplayer.text = "GOLD PER CLICK: " + DataController.getInstance ().getGoldPerClick ();
 		goldPerSecDisplayer.text = "GOLD PER SEC: " + DataController.getInstance ().getGoldPerSec ();
+	}
+
+	public void changeBottomView(string tag) {
+		for (int i = 0; i < panelList.Count; i++) {
+			GameObject obj = (GameObject)panelList [i];
+
+			if (obj.tag.Equals (tag)) {
+				obj.gameObject.SetActive (true);
+			} else {
+				obj.gameObject.SetActive (false);
+			}
+		}
 	}
 }
---
+++ Assets/Scripts/ViewChangeButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViewChangeButton : MonoBehaviour {
+	public void viewChange() {
+		string name = this.tag;
+
+		name = name.Replace ("Button", "Panel");
+		UIManager.getInstance ().changeBottomView (name);
+	}
+}
