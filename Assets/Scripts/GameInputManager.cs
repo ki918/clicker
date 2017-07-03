58cec651cb1fa11a68b9754366760658e4f99f26
LHS <hslee@digicaps.com>
2017-07-03 14:16:51 +0900

    LHS
    뒤로가기 키 종료 추가

--- Assets/Scripts/GameInputManager.cs
+++ Assets/Scripts/GameInputManager.cs
@@ -4,6 +4,12 @@ using UnityEngine.EventSystems;
 
 public class GameInputManager : MonoBehaviour {
 	public void Update() {
+		if (Application.platform == RuntimePlatform.Android) {
+			if (Input.GetKey (KeyCode.Escape)) {
+				Application.Quit ();
+			}
+		}
+
 		if (Input.GetMouseButtonDown (0)) {
 			if (EventSystem.current.IsPointerOverGameObject () == false) {
 				DataController.getInstance ().TouchScreen (Input.mousePosition);
