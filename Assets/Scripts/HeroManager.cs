3cb4c5bfa0f1943067d321187b80de73e512202a
ki918 <darkname@naver.com>
2017-07-08 21:40:03 +0900

    LHS
    주인공 좌/우 이동 추가
    좌/우 끝 부분 충돌 시 아래로 이동 추가
    상/하 충돌 부분은 작업 필요

--- Assets/Scripts/HeroManager.cs
+++ Assets/Scripts/HeroManager.cs
@@ -6,6 +6,10 @@ using UnityEngine.UI;
 public class HeroManager : MonoBehaviour {
 	public static HeroManager instance;
 	public Image heroImg;
+	private int spriteNum = 2;
+	private int heroSpeed = 10;
+	private int upDownSpeed = 0;
+	private bool isRight = true;
 
 	public static HeroManager getInstance() {
 		if (instance == null) {
@@ -22,6 +26,35 @@ public class HeroManager : MonoBehaviour {
 	}
 
 	public void moveHero() {
-		heroImg.sprite = Resources.Load<Sprite> ("Hero/hero_02") as Sprite;
+		Vector3 vec = heroImg.transform.localScale;
+
+		if (isRight) {
+			vec.x = Mathf.Abs (vec.x);
+			heroImg.transform.localScale = vec;
+		} else {
+			vec.x = -Mathf.Abs (vec.x);
+			heroImg.transform.localScale = vec;
+		}
+
+		heroImg.sprite = Resources.Load<Sprite> ("Hero/hero_0" + spriteNum) as Sprite;
+
+		if (upDownSpeed != 0) {
+			heroImg.transform.Translate (heroSpeed, upDownSpeed, 0);
+			upDownSpeed = 0;
+		} else {
+			heroImg.transform.Translate (heroSpeed, upDownSpeed, 0);
+		}
+			
+		spriteNum++;
+
+		if (spriteNum > 6) {
+			spriteNum = 1;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		isRight = !isRight;
+		upDownSpeed = -50;
+		heroSpeed *= -1;
 	}
 }
