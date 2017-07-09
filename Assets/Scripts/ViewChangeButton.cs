1f0f63807d1a8f0439cc5ced7ad3a7fb691b0cbd
ki918 <darkname@naver.com>
2017-07-09 15:55:56 +0900

    LHS
    누락 코드 추가

--- Assets/Scripts/ViewChangeButton.cs
+++ Assets/Scripts/ViewChangeButton.cs
@@ -5,6 +5,7 @@ public class ViewChangeButton : MonoBehaviour {
 	public void viewChange() {
 		string name = this.tag;
 
+		name = name.Replace ("Button", "Panel");
 		UIManager.getInstance ().changeBottomView (name);
 	}
 }
