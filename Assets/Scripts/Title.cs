9e57190ddfb5f1b3e7739b58fc801eb0a3ee02e8
noru <nks150@naver.com>
2017-08-15 03:01:47 +0900

    nks150

--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Title : MonoBehaviour {
+	public Image button; 
 
 	// Use this for initialization
 	void Start () {
-		
+		//String stage = DataController.getInstance ().getSTAGET ();
+		//button.sprite = Resources.Load<Sprite> ("Hero/stage_0" + stage) as Sprite;
 	}
 	
 	// Update is called once per frame
