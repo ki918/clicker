387ab7cb22e44f6fadb77b230daa0d18be1ea330
LHS <hslee@digicaps.com>
2017-08-24 10:28:20 +0900

    LHS
    광고 테스트 기능 추가

---
+++ Assets/Scripts/Manager/ADSManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class ADSManager : MonoBehaviour {
+
+	public void ShowRewardedVideo ()
+	{
+		var options = new ShowOptions();
+		options.resultCallback = HandleShowResult;
+
+		Advertisement.Show("rewardedVideo", options);
+	}
+
+	public void HandleShowResult (ShowResult result)
+	{
+		if(result == ShowResult.Finished) {
+			Debug.Log("Video completed - Offer a reward to the player");
+
+		}else if(result == ShowResult.Skipped) {
+			Debug.LogWarning("Video was skipped - Do NOT reward the player");
+
+		}else if(result == ShowResult.Failed) {
+			Debug.LogError("Video failed to show");
+		}
+	}
+}
